bd61fccce7ea839745a6037426f5b9b162d464a2
tuanlq <tuanlqhvu@gmail.com>
2020-06-29 22:33:04 +0700

    update

---
+++ topic8/chuoitrongpython.py
@@ -0,0 +1,10 @@
+# chuoi nam trong cap "" hoac ''
+
+str1 = "Toi tu hoc lap trinh Python"
+str2 = 'Toi tu hoc lap trinh Python o nha'
+print(str1)
+print(str2)
+str3 = "Ban co thich hoc lap trinh 'Python' khong?"
+str4 = 'Ban co thich hoc lap trinh "Python" khong?'
+print(str3)
+print(str4)
