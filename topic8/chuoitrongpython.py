060ec6a4b96f9a6952ca6be3ce6a3bf4564afd4e
tuanlq <tuanlqhvu@gmail.com>
2020-12-26 15:20:51 +0700

    add opencv numpy

--- topic8/chuoitrongpython.py
+++ topic8/chuoitrongpython.py
@@ -1,10 +1,2 @@
-# chuoi nam trong cap "" hoac ''
-
-str1 = "Toi tu hoc lap trinh Python"
-str2 = 'Toi tu hoc lap trinh Python o nha'
-print(str1)
-print(str2)
-str3 = "Ban co thich hoc lap trinh 'Python' khong?"
-str4 = 'Ban co thich hoc lap trinh "Python" khong?'
-print(str3)
-print(str4)
+import cv2
+print("Hello!")
